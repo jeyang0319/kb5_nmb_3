7dc5835d1fc88bd10618014f70c42f9713051894
Yang Ji Eun <jeyang0319@naver.com>
2024-06-11 14:00:29 +0900

    add fetch

---
+++ household_ledger/src/pages/inoutItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        {{inoutItem.date}}
+        {{inoutItem.amount}}
+    </li>
+</template>
+
+<script setup>
+import {useRouter} from "vue-router";
+import {useInoutListStore} from "../stores/inoutList.js"
+defineProps({
+    inoutItem: {Type:Object, required: true},
+});
+const router = useRouter()
+const inoutListStore = useInoutListStore();
+
+</script>
