18efe053d41d1e0b38920a1ba0dc5f759ef79ee6
gunwoo <wnwn8945@naver.com>
2024-06-14 10:16:55 +0900

    add chart

--- household_ledger/src/pages/report.vue
+++ household_ledger/src/pages/report.vue
@@ -1,48 +1,75 @@
 <template>
     <div>
-        <h2>전체</h2>
+        <h2>Total</h2>
         <canvas id="myChart"></canvas>
     </div>
 </template>
-<script setup>
-import { onMounted } from 'vue';
-import { Chart as ChartJS, registerables } from 'chart.js';
-ChartJS.register(...registerables);
-onMounted(() => {
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new ChartJS(ctx, {
-        type: 'bar',
-        data: {
-            labels: [
-                '1월',
-                '2월',
-                '3월',
-                '4월',
-                '5월',
-                '6월',
-                '7월',
-                '8월',
-                '9월',
-                '10월',
-                '11월',
-                '12월',
-            ], // x축
-            datasets: [
-                // y축
-                {
-                    label: '월말 결산',
-                    data: [20, 100, 100000, 2999999, 500000000],
-                    borderWidth: 1,
-                },
-            ],
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
+<script>
+import Chart from 'chart.js/auto';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+export default {
+    mounted() {
+        const ctx = document.getElementById('myChart');
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [
+                    '1월',
+                    '2월',
+                    '3월',
+                    '4월',
+                    '5월',
+                    '6월',
+                    '7월',
+                    '8월',
+                    '9월',
+                    '10월',
+                    '11월',
+                    '12월'
+                ], //x축
+                datasets: [
+                    //y축
+                    {
+                        label : "수입" ,label: "지출",
+                        data: [13333,333313113],
+                        borderWidth: 1,
+                    },
+                ],
+                datasets: [
+                    //y축
+                    {
+                        label : "지출",
+                        data: [111.222,1111.],
+                        borderWidth: 2,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
                 },
             },
-        },
-    });
-});
+        });
+    },
+};
 </script>
